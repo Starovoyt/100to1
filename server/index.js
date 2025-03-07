e75ec245560855f2ce40b46d050941c89f877772
Starovoyt Sergey <sstarovoyt@sportmasterlab.net>
2025-03-07 12:10:05 +0300

    Указан адрес подключения к БД через 127.0.0.1 вместо localhost, чтобы соединение явно было через IPv4

--- server/index.js
+++ server/index.js
@@ -25,10 +25,9 @@ const io = new Server(server, {
 });
 
 mongoose
-    .connect('mongodb://localhost:27017/100to1', {
+    .connect('mongodb://127.0.0.1:27017/100to1', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-
     })
     .then(() => console.log('MongoDB database Connected...'))
     .catch((err) => console.log(err));
